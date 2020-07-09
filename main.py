52c7a694640b5b71764cbd7f80271a1fd774af9e
Camilo Suarez <juan.suarez.quinones@unillanos.edu.co>
2020-07-09 17:21:50 -0500

    crear botones

--- main.py
+++ main.py
@@ -1,7 +1,24 @@
 import toga
 
+#funcionalidad al precionar widget es un disparador para que funcione un evento
+def on_press(widget):
+    print("Hola mundo utilizando toga")
+
+def new_button():
+    button = toga.Button("Hola mundo", on_press=on_press)#(lo que dira el boton, on_press=)
+    button.style.padding = 20
+    button.style.font_size = 14
+    button.style.width = 200
+    
+    return button
+
 def build(app):
+    #creamos nuestra caja
     box = toga.Box()
+    #instaciamos el boton
+    boton1 = new_button()
+    #agregamos el boton a la caja
+    box.add(boton1)
     return box
 
 #crear ventana
