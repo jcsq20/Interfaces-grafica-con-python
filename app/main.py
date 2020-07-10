2ecbabe82777347977d12e17151e07d17e7518b3
Camilo Suarez <juan.suarez.quinones@unillanos.edu.co>
2020-07-09 20:33:39 -0500

    split y container
    separar la pantalla y agregar contenido

--- app/main.py
+++ app/main.py
@@ -1,5 +1,5 @@
 import toga
-from app.consts import *
+from consts import *
 class PokeDex(toga.App):
     def __init__(self, title, id):
         toga.App.__init__(self, title, id)
@@ -7,19 +7,30 @@ class PokeDex(toga.App):
         self.title = title
         self.size = (WIDTH, HEIGHT)
 
+        self.heading = ["Nombre"]
+        self.data = ["Pyhton", "Rubi", "sql"]
+
+        self.create_elements()
+
     def startup(self):
         self.main_window = toga.MainWindow("main", title=self.title,
                                             size=self.size)#ancho y alto en px
 
         box = toga.Box()
-        self.main_window.content = box
-        self.main_window.show()
-    def create_elements(self):
-        pass
 
+        split = toga.SplitContainer()
+        split.content = [self.table, box]
+
+        self.main_window.content = split
+        self.main_window.show()
+
+    def create_elements(self):
+        self.create_table()
+    
+    def create_table(self):
+        self.table = toga.Table(self.heading, data=self.data)
 
     #CALLBACKS
-
 if __name__ == "__main__":
     Pokedex = PokeDex("Pokedex", "com.codigofacilito.Pokedex")
     Pokedex.main_loop()
